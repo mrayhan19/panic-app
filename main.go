ad224cee69dedb5defe154709a6d15a78b551ab0
Mohammad Rayhan <mroyhan95@gmail.com>
2025-06-25 08:48:38 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type User struct {
+	Name string
+}
+
+func panicHandler(w http.ResponseWriter, r *http.Request) {
+	var u *User
+	fmt.Fprintf(w, "User: %s", u.Name) // Ini akan panic karena u == nil
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "OK")
+}
+
+func main() {
+	http.HandleFunc("/panic", panicHandler)
+	http.HandleFunc("/healthz", healthHandler)
+
+	fmt.Println("Server running on :8080")
+	http.ListenAndServe(":8080", nil)
+}
